627fc8ff4da24af09da4cc88f4121c66fd1f472d
lalit-003 <gahlawatl7@gmail.com>
2020-11-02 07:13:57 +0530

    START commit

---
+++ AddressBook/src/test/java/com/bridgelabz/addressbook/AddressBook/Testing_AddressBook.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.addressbook.AddressBook;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class Testing_AddressBook
+{
+        public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
