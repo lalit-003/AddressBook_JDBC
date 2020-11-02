eceddb08abc0591fafdf697a98db9496d4f51bdd
lalit-003 <gahlawatl7@gmail.com>
2020-11-02 16:37:40 +0530

    added functionality to update city of contact and sync it with database

--- AddressBook/src/main/java/com/bridgelabz/addressbook/AddressBook/AddressBook_DB.java
+++ AddressBook/src/main/java/com/bridgelabz/addressbook/AddressBook/AddressBook_DB.java
@@ -2,6 +2,7 @@ package com.bridgelabz.addressbook.AddressBook;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 
@@ -9,11 +10,45 @@ import com.mysql.cj.jdbc.Driver;
 
 public class AddressBook_DB {
 
-	AddressBook_DataBaseService addressBookService = new AddressBook_DataBaseService();
+	 private AddressBook_DataBaseService addressBookService;
+	List<AddressBookData> addressBookList = new ArrayList<>();
 	
-	public List<AddressBookData> readData() {
-		return addressBookService.readData();
+	public AddressBook_DB() {
+		addressBookService = AddressBook_DataBaseService.getInstance();
 	}
 
-   	
+	public AddressBook_DB(List<AddressBookData> addressBookList) {
+		this();
+		this.addressBookList = addressBookList;
+	}
+	
+	public List<AddressBookData> readData() {
+		this.addressBookList = addressBookService.readData(); 
+		return  addressBookList;
+	}
+
+	public void updateAddressBookData(String firstname, String city)
+	{
+		int result = new AddressBook_DataBaseService().updateEmployeeSalaryResult(firstname, city);
+		if(result == 0) return;
+		AddressBookData AddressBookData = this.getAddressBookData(firstname);
+		if(AddressBookData != null) AddressBookData.setCity(city);
+	}
+	
+	private AddressBookData getAddressBookData(String firstname) {
+      AddressBookData addressBookData ;
+      addressBookData = this.addressBookList.stream()
+    		             .filter(addressBookEntry  ->  (addressBookEntry.getFirstname()).equals(firstname))
+    		             .findFirst()
+    		             .orElse(null);
+		return addressBookData;
+	}
+
+	public boolean checkAddressBookDataSyncWithDB(String firstname) {
+		try {
+			return addressBookService.getAddressBookData(firstname).get(0).getFirstname().equals(getAddressBookData(firstname).getFirstname());
+		} catch (IndexOutOfBoundsException e) {
+		}
+		return false;
+	}
 }
--- AddressBook/src/main/java/com/bridgelabz/addressbook/AddressBook/AddressBook_DataBaseService.java
+++ AddressBook/src/main/java/com/bridgelabz/addressbook/AddressBook/AddressBook_DataBaseService.java
@@ -2,6 +2,7 @@ package com.bridgelabz.addressbook.AddressBook;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,6 +10,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AddressBook_DataBaseService {
+	
+	private PreparedStatement addressBookDataStatement;
+	private static AddressBook_DataBaseService addressBookDBService;
+	
+	public static AddressBook_DataBaseService getInstance() {
+		if (addressBookDBService == null)
+			addressBookDBService = new AddressBook_DataBaseService();
+		return addressBookDBService;
+	}
+	
 
 	private Connection getConnection() throws SQLException {
 		String jdbcURL = "jdbc:mysql://localhost:3306/address_book_service?useSSL=false";
@@ -40,5 +51,63 @@ public class AddressBook_DataBaseService {
 			e.printStackTrace();
 		}
 		return addressBookList;
+	}
+
+	public int updateEmployeeSalaryResult(String firstname, String city) {
+		String sql = String.format("update address_table set city ='%s' where firstname = '%s';", city,firstname);
+		try(Connection connection = this.getConnection();)
+		{
+		Statement statement = connection.createStatement();
+		return statement.executeUpdate(sql);
+				}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return 0;
+	}
+	
+	public List<AddressBookData> getAddressBookData(String firstname) {
+		List<AddressBookData> addressBookList = new ArrayList<>();
+		if (addressBookDataStatement == null)
+		{
+			prepareStatementForAddressBookData();
+		}
+		try {
+		addressBookDataStatement.setString(1, firstname);
+		ResultSet resultSet = addressBookDataStatement.executeQuery();
+		while (resultSet.next()) {
+			addressBookList.add(new AddressBookData(resultSet.getString("firstname"), resultSet.getString("lastname"),
+					resultSet.getString("address"),resultSet.getString("city"),resultSet.getString("state"),resultSet.getInt("zip"),
+					resultSet.getInt("phonenumber"),resultSet.getString("email") ));
+         		}
+						} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return addressBookList;
+		}
+
+
+	private void prepareStatementForAddressBookData() {
+		try {
+			Connection connection = getConnection();
+			String sql = "SELECT * FROM address_table WHERE firstname = ?";
+			addressBookDataStatement = connection.prepareStatement(sql);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int updateCityUsingSQL(String firstname, String city) {
+		String sql = "UPDATE address_table SET city = ? WHERE firstname = ? ";
+		try (Connection connection = getConnection()) {
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			preparedStatement.setString(1, city);
+			preparedStatement.setString(2, firstname);
+			return preparedStatement.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return 0;
 	}
 }
